371effb0ab37c06176c95e1589483ed28a8aad41
Kamil Ochnik <ilmak@o2.pl>
2019-03-06 15:48:08 +0100

    zmiany zmiany ..

--- src/main/java/kalkulator/KalkulatorController.java
+++ src/main/java/kalkulator/KalkulatorController.java
@@ -1,17 +1,30 @@
 package kalkulator;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class KalkulatorController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Tu będzie kalkulator!!!!";
+
+
+
+
+    @RequestMapping("/kalkulator_form")
+    public String obliczenia (
+
+    ) {
+
+        Kalkulator kalkulator = new Kalkulator (20.00, 10.00);
+        kalkulator.dodawanieLiczb(10.00,15.00);
+        System.out.println(kalkulator.dodawanieLiczb(10.00,15.00));
+        return "costam";
     }
 
-    @RequestMapping("/kalkulator")
+
+    @RequestMapping("/")
     public String wyswietlanie (){
         return "kalkulator_form";
     }
