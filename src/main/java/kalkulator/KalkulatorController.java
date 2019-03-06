38eabf771240108fea9a3aba0edf46f861db73d0
Kamil Ochnik <ilmak@o2.pl>
2019-03-06 10:27:54 +0100

    wybrałem pliki które maja być zapisane na GitHubie

---
+++ src/main/java/kalkulator/KalkulatorController.java
@@ -0,0 +1,14 @@
+package kalkulator;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class KalkulatorController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Tu będzie kalkulator!!!!";
+    }
+
+}
