d39ccb7e8c5dc43f92c09161a50455636f78d50a
Kamil Ochnik <ilmak@o2.pl>
2019-03-07 13:47:12 +0100

    temat dzielenia przez zero cd.. zrobie commita aby zapisac ten stan

--- src/main/java/kalkulator/Kalkulator.java
+++ src/main/java/kalkulator/Kalkulator.java
@@ -14,11 +14,13 @@ public class Kalkulator {
     public void mnozenieLiczb (Double pierwszaLiczba, Double drugaLiczba ){
         wynikObliczen = pierwszaLiczba * drugaLiczba;
     }
-    public void dzielenieLiczb (Double pierwszaLiczba, Double drugaLiczba ){
-        if (drugaLiczba ==0){
-            wynikObliczen = Double.parseDouble("Nie mozna dzielic przez ZERO!!!");;
-        } else {
+    public void dzielenieLiczb (Double pierwszaLiczba, Double drugaLiczba ) throws NullPointerException  {
 
+       if (drugaLiczba ==0){
+            wynikObliczen = Double.parseDouble("Nie mozna dzielic przez ZERO!!!");
+        } else if (pierwszaLiczba == null || drugaLiczba == null) {
+            wynikObliczen = Double.parseDouble("costam");
+        } else {
             wynikObliczen = pierwszaLiczba / drugaLiczba;
         }
     }
