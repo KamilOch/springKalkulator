c7ea8220cf20e15b8e40c9b8e7e996ee76a68a07
Kamil Ochnik <ilmak@o2.pl>
2019-03-08 09:03:54 +0100

    temat dzielenia przez zero, jest juz lepiej

--- src/main/java/kalkulator/Kalkulator.java
+++ src/main/java/kalkulator/Kalkulator.java
@@ -1,37 +1,31 @@
 package kalkulator;
 
 public class Kalkulator {
-    private Double wynikObliczen;
+    private String wynikObliczen;
 
 
     public void dodawanieLiczb (Double pierwszaLiczba, Double drugaLiczba ){
-        wynikObliczen = pierwszaLiczba + drugaLiczba;
+        wynikObliczen = Double.toString(pierwszaLiczba + drugaLiczba);
     }
 
     public void odejmowanieLiczb (Double pierwszaLiczba, Double drugaLiczba ){
-        wynikObliczen = pierwszaLiczba - drugaLiczba;
+        wynikObliczen = Double.toString(pierwszaLiczba - drugaLiczba);
     }
     public void mnozenieLiczb (Double pierwszaLiczba, Double drugaLiczba ){
-        wynikObliczen = pierwszaLiczba * drugaLiczba;
+        wynikObliczen = Double.toString(pierwszaLiczba * drugaLiczba);
     }
     public void dzielenieLiczb (Double pierwszaLiczba, Double drugaLiczba ){
-/*
-        if (pierwszaLiczba != null && !pierwszaLiczba.isEmpty()){
-            wynikObliczen = pierwszaLiczba / drugaLiczba;
-        } else {
-            wynikObliczen = Double.parseDouble("Nie mozna dzielic przez ZERO!!!");
-        }
-*/
 
-  //    /*
        if (drugaLiczba ==0){
-            wynikObliczen = Double.valueOf("Nie mozna dzielic przez ZERO!!!");
-        } else if (pierwszaLiczba == null || drugaLiczba == null) {
-            wynikObliczen = Double.valueOf("costam");
+            wynikObliczen = "Nie mozna dzielic przez ZERO!!!";
+        } else if (pierwszaLiczba == null) {
+           wynikObliczen = "Nie podałeś pierwszej liczby!";
+        } else if (drugaLiczba == null) {
+           wynikObliczen = "Nie podałeś drugiej liczby!";
         } else {
-            wynikObliczen = pierwszaLiczba / drugaLiczba;
+            wynikObliczen = Double.toString(pierwszaLiczba / drugaLiczba);
         }
-    //    */
+
     }
 
 
